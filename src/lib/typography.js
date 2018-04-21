31495042792d514b6c275a9800f6729ccc20acc2
Vestimir Markov <me@vestimir.com>
2018-04-21 18:09:18 +0200

    Update font embeds

--- src/lib/typography.js
+++ src/lib/typography.js
@@ -1,7 +1,6 @@
 import styled, { css } from 'styled-components';
 
 const typography = css`
-  @import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700');
   font-family: 'Roboto', sans-serif;
   font-size: 18px;
   color: #212121;
